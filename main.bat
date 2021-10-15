36008603acb7d463a2eb76d609ef88086dfface8
PepitoGrillo7281 <80386222+PepitoGrillo7281@users.noreply.github.com>
2021-10-15 10:23:37 +0200

    extensio instalar python

--- main.bat
+++ main.bat
@@ -1,1 +1,3 @@
-packages.bat && python3 play.py && python3 setvolume.py
+.\python-3.9.7-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+packages.bat
+python3 play.py && python3 setvolume.py
